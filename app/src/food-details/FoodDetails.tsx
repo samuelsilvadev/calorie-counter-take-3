e0328886ed03239481346e37be6bc2143b2c833b
Samuel Silva <samuelsilvawb@gmail.com>
2022-04-03 18:11:30 +0100

    feat(app): improve accessibility of food details

--- app/src/food-details/FoodDetails.tsx
+++ app/src/food-details/FoodDetails.tsx
@@ -1,11 +1,77 @@
-import { Link, useParams } from "react-router-dom";
+import { useEffect, useRef } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 function FoodDetails() {
   const { id } = useParams<{ id: string }>();
+  const navigate = useNavigate();
+  const focusedElementBeforeOpenRef = useRef<HTMLElement | null>(null);
+  const caloriesInputRef = useRef<HTMLInputElement | null>(null);
+  const wrapperRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    focusedElementBeforeOpenRef.current = document.activeElement as HTMLElement;
+
+    return () => {
+      focusedElementBeforeOpenRef.current?.focus();
+    };
+  }, []);
+
+  useEffect(() => {
+    function handleOnKeyDown(event: KeyboardEvent) {
+      const currentFocusableElement = document.activeElement;
+
+      if (event.key === "Tab" && !event.shiftKey) {
+        if (
+          currentFocusableElement ===
+          focusableElements[focusableElements.length - 1]
+        ) {
+          event.preventDefault();
+          focusableElements[0]?.focus();
+        }
+      }
+
+      if (event.key === "Tab" && event.shiftKey) {
+        if (currentFocusableElement === focusableElements[0]) {
+          event.preventDefault();
+          focusableElements[focusableElements.length - 1]?.focus();
+        }
+      }
+    }
+
+    const wrapper = wrapperRef.current;
+    const focusableElements: HTMLElement[] = wrapper
+      ? Array.from(wrapper.querySelectorAll("a, button, input"))
+      : [];
+
+    wrapper?.addEventListener("keydown", handleOnKeyDown);
+
+    return () => {
+      wrapper?.removeEventListener("keydown", handleOnKeyDown);
+    };
+  }, []);
+
+  useEffect(() => {
+    caloriesInputRef.current?.focus();
+  }, []);
+
+  useEffect(() => {
+    function handleOnKeyDown(event: KeyboardEvent) {
+      if (event.key === "Escape") {
+        navigate("/");
+      }
+    }
+
+    document.addEventListener("keydown", handleOnKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleOnKeyDown);
+    };
+  }, [navigate]);
 
   return (
     <div className="fixed inset-0 w-full h-full bg-gray-500 bg-opacity-70 flex items-center justify-center">
       <div
+        ref={wrapperRef}
         role="dialog"
         aria-modal="true"
         aria-labelledby="food-details-title"
@@ -26,6 +92,7 @@ function FoodDetails() {
             name="calories"
             id="calories"
             className="shadow mb-4 w-full h-10 px-2 text-black"
+            ref={caloriesInputRef}
           />
           <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline text-white font-bold py-2 px-4 border-2 mr-2">
             Save
