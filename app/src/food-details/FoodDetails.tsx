1386fe4aa09fb9adb781fd4fd2c5496ec52be801
Samuel Silva <samuelsilvawb@gmail.com>
2022-04-04 22:28:06 +0100

    feat(app): calculate amount of consumed calories

--- app/src/food-details/FoodDetails.tsx
+++ app/src/food-details/FoodDetails.tsx
@@ -1,4 +1,10 @@
-import { useEffect, useRef } from "react";
+import {
+  ChangeEventHandler,
+  FormEventHandler,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { useQuery } from "react-query";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { Food } from "../types/Food";
@@ -20,13 +26,34 @@ type TFoodResponse = {
 
 function FoodDetails() {
   const { id } = useParams<{ id: string }>();
+  const navigate = useNavigate();
   const { data } = useQuery<TFoodResponse | null>(["food", id], () =>
     id ? getFood(id) : null
   );
-  const navigate = useNavigate();
+  const food = data?.data;
   const focusedElementBeforeOpenRef = useRef<HTMLElement | null>(null);
   const caloriesInputRef = useRef<HTMLInputElement | null>(null);
   const wrapperRef = useRef<HTMLDivElement | null>(null);
+  const [calculatedCalories, setCalculatedCalories] = useState<number | null>(
+    null
+  );
+  const [consumedCalories, setConsumedCalories] = useState<string>("");
+
+  const handleOnChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const value = event.target.value;
+
+    setConsumedCalories(value);
+
+    if (!value) {
+      setCalculatedCalories(null);
+    }
+  };
+
+  const handleOnSubmit: FormEventHandler = (event) => {
+    event.preventDefault();
+
+    setCalculatedCalories(Number(consumedCalories) * (food?.calories ?? 1));
+  };
 
   useEffect(() => {
     focusedElementBeforeOpenRef.current = document.activeElement as HTMLElement;
@@ -88,8 +115,6 @@ function FoodDetails() {
     };
   }, [navigate]);
 
-  const food = data?.data;
-
   return (
     <div className="fixed inset-0 w-full h-full bg-gray-500 bg-opacity-70 flex items-center justify-center">
       <div
@@ -105,17 +130,28 @@ function FoodDetails() {
         >
           Food Details for: {food?.name}
         </h2>
-        <form>
-          <label htmlFor="calories" className="block  text-sm font-bold mb-2">
+        <form onSubmit={handleOnSubmit}>
+          <label
+            htmlFor="consumedCalories"
+            className="block  text-sm font-bold mb-2"
+          >
             Amount of {food?.name} consumed:
           </label>
           <input
-            type="text"
-            name="calories"
-            id="calories"
+            required
+            type="number"
+            name="consumedCalories"
+            id="consumedCalories"
             className="shadow mb-4 w-full h-10 px-2 text-black"
             ref={caloriesInputRef}
+            onChange={handleOnChange}
+            value={consumedCalories}
           />
+          {typeof calculatedCalories === "number" && (
+            <p className="p-3 border-2 mb-4">
+              You have consumed {calculatedCalories} calories
+            </p>
+          )}
           <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline text-white font-bold py-2 px-4 border-2 mr-2">
             Calculate calories
           </button>
